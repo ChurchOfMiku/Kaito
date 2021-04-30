18a2923ab5124dabc2d7b2a62697bdca96cc6b1d
Mats A <mats@mats.sh>
2021-04-30 14:39:22 +0200

    make the shell.nix pure

--- shell.nix
+++ shell.nix
@@ -1,8 +1,15 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import <nixpkgs> { } }:
 
 pkgs.mkShell {
   buildInputs = with pkgs; [
+    # Rust
+    cargo
+    rustc
+    rustfmt
+    clang
+    # Deps
     pkg-config
+    stdenv.cc.libc
     lua5_4
     openssl
     sqlite
@@ -10,20 +17,11 @@ pkgs.mkShell {
     libwebp
     ffmpeg
     youtube-dl
-    (graphicsmagick.overrideAttrs (
-      oldAttrs: rec {
-        with_windows_font_dir = "${pkgs.corefonts}/share/fonts/truetype";
-        configureFlags = oldAttrs.configureFlags ++ [
-          "--with-librsvg"
-        ];
-        buildInputs = oldAttrs.buildInputs ++ [
-          pkgs.gnome3.librsvg
-        ];
-      }
-    ))
+    graphicsmagick
   ];
 
   shellHook = ''
-    export LIBCLANG_PATH="${pkgs.llvmPackages.libclang}/lib";
+    export LIBCLANG_PATH="${pkgs.llvmPackages.libclang}/lib"
+    export BINDGEN_EXTRA_CLANG_ARGS="-I${pkgs.stdenv.cc.libc}/include"
   '';
 }
