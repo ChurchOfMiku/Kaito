4909a205184b3c1f95a4a655d8184b48961dd742
Mats A <mats@mats.sh>
2021-05-29 18:02:59 +0200

    improve sed

--- lua/bot/modules/sed.lua
+++ lua/bot/modules/sed.lua
@@ -5,14 +5,16 @@ hooks.add("message", "sed", function(msg)
         return
     end
 
-    local find, replace = string.match(msg.content, "^sed/(.*)/(.*)/$")
+    local find_g, replace = string.match(msg.content, "^sed/(.*)/(.*)/g$")
+    local find, replace2 = string.match(msg.content, "^sed/(.*)/(.*)/?$")
+    local replace = replace or replace2
 
-    if find then
+    if find_g or find then
         for i=channel_buffer:get_size(), 1, -1  do
             local prev_msg = channel_buffer:get(i)
             
-            if string.find(prev_msg.content, find) then
-                local replaced = string.gsub(prev_msg.content, find, replace)
+            if string.find(prev_msg.content, find_g or find) then
+                local replaced = find_g and string.gsub(prev_msg.content, find_g, replace) or string.gsub(prev_msg.content, find, replace, 1)
                 msg:reply(msg.channel:escape_text(prev_msg.author.nick) .. ": " .. msg.channel:escape_text(replaced))
 
                 break
