13db86e435fab050e49bd31478c81bf3a7134cc6
Mats A <mats@mats.sh>
2021-05-30 13:04:49 +0200

    add cursed muxing for media extraction

--- src/modules/utils.rs
+++ src/modules/utils.rs
@@ -1,7 +1,9 @@
 use anyhow::Result;
 use async_mutex::Mutex;
 use lru::LruCache;
+use rand::{distributions, Rng};
 use std::sync::Arc;
+use tokio::fs;
 
 use super::{Module, ModuleKind};
 use crate::{
@@ -49,11 +51,43 @@ impl Module for UtilsModule {
     async fn message(&self, msg: Arc<dyn Message<impl Service>>) -> Result<()> {
         lazy_static::lazy_static! {
             static ref REDDIT_RE: regex::Regex = regex::Regex::new(r#"https?://(?:old.|www.)?reddit.com/.+(?: )?"#).unwrap();
+            static ref TIKTOK_RE: regex::Regex = regex::Regex::new(r#"https?://(?:www.)?tiktok.com/.+(?: )?"#).unwrap();
             static ref TWITTER_RE: regex::Regex = regex::Regex::new(r#"https?://(?:www.)?twitter.com/.+/status(?:es)?/(\d+)(?:.+ )?"#).unwrap();
         }
 
-        let mut matches: Vec<_> = REDDIT_RE.captures_iter(msg.content()).collect();
-        matches.append(&mut TWITTER_RE.captures_iter(msg.content()).collect());
+        #[derive(Clone, Copy)]
+        enum MediaService {
+            Reddit,
+            Tiktok,
+            Twitter,
+        }
+
+        impl MediaService {
+            pub fn should_download(self) -> bool {
+                match self {
+                    MediaService::Reddit => true,
+                    MediaService::Tiktok => true,
+                    _ => false,
+                }
+            }
+        }
+
+        let mut matches: Vec<_> = REDDIT_RE
+            .captures_iter(msg.content())
+            .map(|cap| (MediaService::Reddit, cap))
+            .collect();
+        matches.append(
+            &mut TIKTOK_RE
+                .captures_iter(msg.content())
+                .map(|cap| (MediaService::Tiktok, cap))
+                .collect(),
+        );
+        matches.append(
+            &mut TWITTER_RE
+                .captures_iter(msg.content())
+                .map(|cap| (MediaService::Twitter, cap))
+                .collect(),
+        );
 
         if !matches.is_empty() {
             let channel = msg.channel().await?;
@@ -66,35 +100,78 @@ impl Module for UtilsModule {
 
             if extract_media_urls {
                 let mut out = Vec::new();
+                let mut attachment = None;
 
-                for status_match in matches {
-                    let output = tokio::process::Command::new("youtube-dl")
-                        .arg("-g")
-                        .arg("-f")
-                        .arg("best")
-                        .arg("--no-warnings")
-                        .arg(status_match.get(0).unwrap().as_str())
-                        .output()
-                        .await;
-
-                    match output {
-                        Ok(output) => {
-                            //println!("{}", String::from_utf8_lossy(&output.stderr).to_string());
-                            let url = String::from_utf8_lossy(&output.stdout).to_string();
-                            if !url.is_empty() {
-                                out.push(url);
-                            }
+                for (service, status_match) in matches {
+                    if service.should_download() {
+                        if attachment.is_some() {
+                            continue;
                         }
-                        Err(err) => println!(
-                            "Error from youtube-dl twitter extraction: {}",
-                            err.to_string()
-                        ),
+
+                        let name: String = rand::thread_rng()
+                            .sample_iter(&distributions::Alphanumeric)
+                            .take(6)
+                            .map(char::from)
+                            .collect();
+
+                        let out_path = std::env::temp_dir().join(format!("{}.mp4", name));
+
+                        // TODO: Move to pipeing to stdout when youtube-dl can support do it after muxing
+                        tokio::process::Command::new("youtube-dl")
+                                .args(&[
+                                    "-f", "bestvideo[filesize<8MB]+bestaudio[filesize<2MB]/best/bestvideo+bestaudio",
+                                    "--merge-output-format", "mp4",
+                                    "--ignore-config",
+                                    "--no-playlist",
+                                    "--no-warnings",
+                                    status_match.get(0).unwrap().as_str(),
+                                    "-o", &out_path.to_string_lossy()
+                                    ])
+                                .output().await?;
+
+                        if out_path.exists() {
+                            attachment = Some(fs::read(&out_path).await?);
+                            fs::remove_file(&out_path).await?;
+                        }
+                    } else {
+                        let output = tokio::process::Command::new("youtube-dl")
+                            .arg("-g")
+                            .arg("-f")
+                            .arg("best")
+                            .arg("--no-warnings")
+                            .arg(status_match.get(0).unwrap().as_str())
+                            .output()
+                            .await;
+
+                        match output {
+                            Ok(output) => {
+                                //println!("{}", String::from_utf8_lossy(&output.stderr).to_string());
+                                let url = String::from_utf8_lossy(&output.stdout).to_string();
+                                if !url.is_empty() {
+                                    out.push(url);
+                                }
+                            }
+                            Err(err) => println!(
+                                "Error from youtube-dl twitter extraction: {}",
+                                err.to_string()
+                            ),
+                        }
                     }
                 }
 
-                if !out.is_empty() {
+                if !out.is_empty() || attachment.is_some() {
                     let reply_msg = channel
-                        .send(out.join("\n"), MessageSettings::default())
+                        .send(
+                            out.join("\n"),
+                            if let Some(attachment) = attachment {
+                                MessageSettings {
+                                    attachments: vec![("video.mp4".into(), attachment)],
+                                    ..MessageSettings::default()
+                                }
+                            } else {
+                                MessageSettings::default()
+                            },
+                        )
                         .await?;
 
                     self.last_generated_messages
