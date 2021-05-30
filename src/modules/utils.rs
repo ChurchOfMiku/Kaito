8fc4b92450aaf6ff32395b209a936c0962a23a69
Mats A <mats@mats.sh>
2021-05-30 14:10:41 +0200

    add vm.tiktok.com to the tiktok regex

--- src/modules/utils.rs
+++ src/modules/utils.rs
@@ -51,7 +51,7 @@ impl Module for UtilsModule {
     async fn message(&self, msg: Arc<dyn Message<impl Service>>) -> Result<()> {
         lazy_static::lazy_static! {
             static ref REDDIT_RE: regex::Regex = regex::Regex::new(r#"https?://(?:old.|www.)?reddit.com/.+(?: )?"#).unwrap();
-            static ref TIKTOK_RE: regex::Regex = regex::Regex::new(r#"https?://(?:www.)?tiktok.com/.+(?: )?"#).unwrap();
+            static ref TIKTOK_RE: regex::Regex = regex::Regex::new(r#"https?://(?:www.|vm.)?tiktok.com/.+(?: )?"#).unwrap();
             static ref TWITTER_RE: regex::Regex = regex::Regex::new(r#"https?://(?:www.)?twitter.com/.+/status(?:es)?/(\d+)(?:.+ )?"#).unwrap();
         }
 
