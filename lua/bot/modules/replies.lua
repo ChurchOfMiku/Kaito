a62f2dab1bf257fddf2ac6fd849c6a04502fd946
jaxon <booga956@gmail.com>
2022-08-08 01:53:21 -0600

    Commented out Replies.lua so lix will untimeout kaito

--- lua/bot/modules/replies.lua
+++ lua/bot/modules/replies.lua
@@ -1,55 +1,57 @@
-local function count_caps(str)
-    local _, caps = string.gsub(str, "[A-Z]", "")
-    return caps
-end
+-- Comment out to untimeout kaito
 
-local function count_chars(str)
-    local _, chars = string.gsub(str, "[A-Za-z]", "")
-    return chars
-end
+-- local function count_caps(str)
+--     local _, caps = string.gsub(str, "[A-Z]", "")
+--     return caps
+-- end
 
-local REPLIES = {
-    ["thanks kaito"] = { "You're welcome", "No problem!", "Nya~!" },
-    ["based kaito"] = {"😎", "💪", "Nya~!"},
-    ["fuck kaito"] = {"Yes, uwu", "Yes, fuck me", "Nya~!!!", "Myaa~!!"}
-}
+-- local function count_chars(str)
+--     local _, chars = string.gsub(str, "[A-Za-z]", "")
+--     return chars
+-- end
 
-hooks.add("message", "replies", function(msg)
-    for check, replies in pairs(REPLIES) do
-        local dist = string.levenshtein(string.sub(string.lower(msg.content), 1, #check), check)
+-- local REPLIES = {
+--     ["thanks kaito"] = { "You're welcome", "No problem!", "Nya~!" },
+--     ["based kaito"] = {"😎", "💪", "Nya~!"},
+--     ["fuck kaito"] = {"Yes, uwu", "Yes, fuck me", "Nya~!!!", "Myaa~!!"}
+-- }
 
-        if dist < 3 then
-            local reply = replies[math.random(1, #replies)]
-            local reply_chars = count_chars(reply)
+-- hooks.add("message", "replies", function(msg)
+--     for check, replies in pairs(REPLIES) do
+--         local dist = string.levenshtein(string.sub(string.lower(msg.content), 1, #check), check)
 
-            local caps = math.floor(count_caps(msg.content) * (reply_chars / count_chars(msg.content)) + 0.5)
+--         if dist < 3 then
+--             local reply = replies[math.random(1, #replies)]
+--             local reply_chars = count_chars(reply)
 
-            if caps == reply_chars then
-                reply = string.upper(reply)
-            elseif caps > 2 then
-                reply = string.lower(reply)
+--             local caps = math.floor(count_caps(msg.content) * (reply_chars / count_chars(msg.content)) + 0.5)
 
-                for i=1, caps do
-                    for _=1, 3 do
-                        local offset = math.random(1, #reply)
-                        local c = string.sub(reply, offset, offset)
-                        if string.match(c, "[a-z]") then
-                            reply = string.sub(reply, 1, offset - 1) .. string.upper(c) .. string.sub(reply, offset + 1, #reply)
+--             if caps == reply_chars then
+--                 reply = string.upper(reply)
+--             elseif caps > 2 then
+--                 reply = string.lower(reply)
 
-                            break
-                        end
-                    end
-                end
-            elseif caps == 0 then
-                reply = string.lower(reply)
-            end
+--                 for i=1, caps do
+--                     for _=1, 3 do
+--                         local offset = math.random(1, #reply)
+--                         local c = string.sub(reply, offset, offset)
+--                         if string.match(c, "[a-z]") then
+--                             reply = string.sub(reply, 1, offset - 1) .. string.upper(c) .. string.sub(reply, offset + 1, #reply)
 
-            for i=1,dist do
-                local offset = math.random(1, #reply)
-                reply = string.sub(reply, 1, offset) .. string.sub(reply, offset + 2, #reply)
-            end
+--                             break
+--                         end
+--                     end
+--                 end
+--             elseif caps == 0 then
+--                 reply = string.lower(reply)
+--             end
 
-            msg:reply(reply)
-        end
-    end
-end)
+--             for i=1,dist do
+--                 local offset = math.random(1, #reply)
+--                 reply = string.sub(reply, 1, offset) .. string.sub(reply, offset + 2, #reply)
+--             end
+
+--             msg:reply(reply)
+--         end
+--     end
+-- end)
